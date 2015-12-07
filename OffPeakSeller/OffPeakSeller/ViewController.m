653a8335edaab50db79458873fd65a0c835ac3b6
ImranNaseem1989 <imran0621mfp@gmail.com>
2015-12-07 16:21:29 +0530

    AFNetworking Added
    There was issue in adding AFNetworking manually in the latest version
    which I have done in previous version.So I added using the Cocoa Pods.

--- OffPeakSeller/OffPeakSeller/ViewController.m
+++ OffPeakSeller/OffPeakSeller/ViewController.m
@@ -12,11 +12,46 @@
 
 @end
 
+//http://119.81.207.44/seller_login.php
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    static NSString * const BaseURLString = @"http://www.raywenderlich.com/demos/weather_sample/";
+    
+
+    NSString *string = [NSString stringWithFormat:@"%@weather.php?format=json", BaseURLString];
+    NSURL *url = [NSURL URLWithString:string];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    // 2
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    operation.responseSerializer = [AFJSONResponseSerializer serializer];
+    
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        // 3
+        NSDictionary *dictionary = (NSDictionary *)responseObject;
+        self.title = @"JSON Retrieved";
+        NSLog(@"Dic===%@",dictionary);
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        
+        // 4
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error Retrieving Weather"
+                                                            message:[error localizedDescription]
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"Ok"
+                                                  otherButtonTitles:nil];
+        [alertView show];
+    }];
+    
+    // 5
+    [operation start];
+    
 }
 
 - (void)didReceiveMemoryWarning {
