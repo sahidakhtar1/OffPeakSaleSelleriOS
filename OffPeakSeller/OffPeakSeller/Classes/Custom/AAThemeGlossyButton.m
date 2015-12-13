a36a8f4a3730696a3aac41486e9e3d755c293b24
sahidakhtar1 <ShahidAkhtar@Shahids-MacBook-Air.local>
2015-12-13 16:52:49 +0530

    Changing Button bg color

--- OffPeakSeller/OffPeakSeller/Classes/Custom/AAThemeGlossyButton.m
+++ OffPeakSeller/OffPeakSeller/Classes/Custom/AAThemeGlossyButton.m
@@ -42,7 +42,8 @@
 //    [self setGlossButtonBackgroundColor:[AAColor sharedInstance].retailerThemeBackgroundColor];
     [self setTitleColor:[AAColor sharedInstance].retailerThemeTextColor forState:UIControlStateNormal];
     [self.layer setCornerRadius:4.0];
-    [self setBackgroundColor:[AAColor sharedInstance].retailerThemeBackgroundColor];
+    [self setBackgroundColor:[AAColor sharedInstance].buttonBgColor];
+    [self.titleLabel setFont:[UIFont fontWithName:[AAAppGlobals sharedInstance].boldFont size:BUTTON_FONTSIZE]];
 }
 
 -(void)changeTheme
